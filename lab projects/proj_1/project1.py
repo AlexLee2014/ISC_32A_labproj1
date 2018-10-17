0dc91a2d8615a4f79f95c7e62ad2642739042e20
Auxiliatrix <Squeakadoodle@gmail.com>
2018-10-17 03:37:03 -0700

    Added commenting

--- lab projects/proj_1/project1.py
+++ lab projects/proj_1/project1.py
@@ -3,6 +3,11 @@ import os
 
 # Define file functions
 def file_printer(directory : str) -> []:
+    """Return the paths to files in a directory.
+
+    Keyword arguments:
+    directory -- the path of the directory to search
+    """
     outfiles = []
     for file in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, file)):
@@ -10,6 +15,11 @@ def file_printer(directory : str) -> []:
     return outfiles
 
 def file_recursive_printer(directory : str) -> []:
+    """Return the paths to files in a directory and all of its sub-directories.
+
+    Keyword arguments:
+    directory -- the path of the directory to search
+    """
     outfiles = []
     for path in os.listdir(directory):
         path = os.path.join(directory, path)
@@ -20,6 +30,13 @@ def file_recursive_printer(directory : str) -> []:
     return outfiles
 
 def search_by_name(filename : str, files : [], slashtype : str) -> []:
+    """Return the paths with a given filename from a given list of files.
+
+    Keyword arguments:
+    filename -- the filename to filter by
+    files -- the list of files to filter
+    slashtype -- the type of slash that is used to separate directories
+    """
     outfiles = []
     for file in files:
         if file.endswith(slashtype + filename):
@@ -27,6 +44,12 @@ def search_by_name(filename : str, files : [], slashtype : str) -> []:
     return outfiles
 
 def search_by_extension(extension : str, files : []) -> []:
+    """Return the paths with a given extension from a given list of files.
+
+    Keyword arguments:
+    extension -- the extension type to filter by
+    files -- the list of files to filter
+    """
     outfiles = []
     for file in files:
         if file.endswith("." + extension):
@@ -34,6 +57,12 @@ def search_by_extension(extension : str, files : []) -> []:
     return outfiles
 
 def search_by_text(text : str, files : []) -> []:
+    """Return the paths to files containing the given text from a given list of files.
+
+    Keyword arguments:
+    text -- the text to search for
+    files -- the list of files to filter
+    """
     outfiles = []
     for file in files:
         infile = open(file, 'r')
@@ -42,6 +71,12 @@ def search_by_text(text : str, files : []) -> []:
     return outfiles
 
 def search_by_max_size(size : int, files : []) -> []:
+    """Return the paths to files smaller than a given limit from a given list of files.
+
+    Keyword arguments:
+    size -- the maximum size to filter by
+    files -- the list of files to filter
+    """
     outfiles = []
     for file in files:
         if os.stat(file).st_size < size:
@@ -49,6 +84,12 @@ def search_by_max_size(size : int, files : []) -> []:
     return outfiles
 
 def search_by_min_size(size : int, files : []) -> []:
+    """Return the paths to files larger than a given limit from a given list of files.
+
+    Keyword arguments:
+    size -- the minimum size to filter by
+    files -- the list of files to filter
+    """
     outfile = []
     for file in files:
         if os.stat(file).st_size > size:
@@ -56,12 +97,22 @@ def search_by_min_size(size : int, files : []) -> []:
     return outfile
 
 def get_first_line(filepath : str) -> str:
+    """Return the first line in a given file.
+
+    Keyword arguments:
+    filepath -- the path to the file to read from
+    """
     infile = open(filepath, 'r')
     line = infile.readline()
     infile.close()
     return line
 
 def duplicate_file(filepath : str):
+    """Duplicate a given file.
+
+    Keyword arguments:
+    filepath -- the path to the file to duplicate
+    """
     infile = open(filepath, 'r')
     lines = infile.read()
     outfile = open(filepath + ".dup", 'w+')
@@ -70,6 +121,11 @@ def duplicate_file(filepath : str):
     outfile.close()
     
 def touch_file(filepath : str):
+    """Set a file's last modified date to the current time.
+
+    Keyword arguments:
+    filepath -- the path to the file to touch
+    """
     infile = open(filepath, 'r')
     infile.read()
     outfile = open(filepath, 'w')
@@ -78,7 +134,12 @@ def touch_file(filepath : str):
     outfile.close()
 
 # Define utility functions
-def get_correct_input(pattern : str) -> str:
+def get_formatted_input(pattern : str) -> str:
+    """Continuously read input until it matches a given format, and return the input's function and argument as a tuple.
+
+    Keyword arguments:
+    pattern -- the regex pattern the input must match
+    """
     line = ""
     while( not re.match(pattern, line) ):
         line = input()
@@ -88,6 +149,13 @@ def get_correct_input(pattern : str) -> str:
         print("ERROR")
 
 def persist(funct, ex : Exception, error : str):
+    """Repeat a given function until a given Exception stops occurring, and print an error each time it does.
+
+    Keyword arguments:
+    funct -- the function to execute
+    ex -- the exception to catch and print an error for
+    error -- the error message to print
+    """
     while(True):
         try:
             return funct()
@@ -95,6 +163,13 @@ def persist(funct, ex : Exception, error : str):
             print(error)
 
 def lexicographical_sort(directory : str, inputfiles : [], slashtype : str) -> []:
+    """Return a list of files sorted lexicographically, with files in the base directory in the beginning.
+
+    Keyword arguments:
+    directory -- the base directory
+    inputfiles -- the files to sort
+    slashtype -- the type of slash that is used to separate directories
+    """
     outputfiles = []
     directfiles = []
     subfiles = []
@@ -103,17 +178,25 @@ def lexicographical_sort(directory : str, inputfiles : [], slashtype : str) -> [
             subfiles.append(file)
         else:
             directfiles.append(file)
-    outputfiles.extend(sorted(directfiles, key=lambda str:str.lower()))
-    outputfiles.extend(sorted(subfiles, key=lambda str:str.lower()))
+    outputfiles.extend(sorted(directfiles))
+    outputfiles.extend(sorted(subfiles))
     return outputfiles
 
 # Define organizational functions
 
 def get_files() -> []:
-    function, argument = get_correct_input("^[DR] .+")
+    """Read input until correctly formatted, then return the files requested by the input"""
+    function, argument = get_formatted_input("^[DR] .+")
     return argument, file_printer(argument) if function == ("D") else file_recursive_printer(argument)
 
-def select(function : str, sortedfiles : []) -> []:
+def select(function : str, argument : str, sortedfiles : []) -> []:
+    """Return a list of files sorted by a given command.
+
+    Keyword arguments:
+    function -- the indicator for an aspect to filter by
+    argument -- the optional argument to filter against
+    sortedfiles -- the files to filter through
+    """
     interestingfiles = []
     if function == "A":
         interestingfiles = sortedfiles
@@ -130,6 +213,12 @@ def select(function : str, sortedfiles : []) -> []:
     return interestingfiles
 
 def operate(function : str, sortedfiles : []):
+    """Perform a specified operation on a given list of files.
+
+    Keyword arguments:
+    function -- the indicator for the operation to execute
+    sortedfiles -- the files to operate on
+    """
     if function == "F":
         for file in sortedfiles:
             try:
@@ -149,18 +238,26 @@ def operate(function : str, sortedfiles : []):
 
 # Run program
 if __name__ == '__main__':
+    # Determine which type of slashes are used in this directory system
     slashtype = os.path.join(os.getcwd(),"")[-1]
     
+    # Continuously attempt to retrieve files until the user input is valid.
     directory, files = persist(get_files, FileNotFoundError, "ERROR")
+    
+    # Display the files in lexicographical order
     sortedfiles = lexicographical_sort(directory, files, slashtype)
     for file in sortedfiles:
         print(file)
 
-    function, argument = get_correct_input("^A$|^[NET] .+|^[<>] \d+$")
-    interestingfiles = select(function, sortedfiles)
+    # Continuously read input until valid, then filter files based on user criteria
+    function, argument = get_formatted_input("^A$|^[NET] .+|^[<>] \d+$")
+    interestingfiles = select(function, argument, sortedfiles)
+    
+    # Display the files in lexicographical order
     sortedfiles = lexicographical_sort(directory, interestingfiles, slashtype)
     for file in sortedfiles:
         print(file)
     
-    function, argument = get_correct_input("^[FDT]$")
+    # Continuously read input until valid, then operate on files based on user criteria
+    function, argument = get_formatted_input("^[FDT]$")
     operate(function, sortedfiles)
